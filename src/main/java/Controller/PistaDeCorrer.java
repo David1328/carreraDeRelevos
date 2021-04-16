10d98a1fd20894fe209bbd85bf46403d70c75746
asantibo <asantibo129@gmail.com>
2021-04-15 22:38:16 -0500

    cambios

--- src/main/java/Controller/PistaDeCorrer.java
+++ src/main/java/Controller/PistaDeCorrer.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package Controller;
+
 import Modelo.Corredor;
 
 /**
@@ -11,54 +12,55 @@ import Modelo.Corredor;
  * @author David
  */
 public class PistaDeCorrer {
+
     public static void main(String[] args) throws InterruptedException {
+        //Creación de objetos en común para cada equipo que se va a usar
         Object equipoUno = new Object();
         Object equipoDos = new Object();
         Object equipoTres = new Object();
-        
-        
-        Corredor corredorUnoA = new Corredor("A",equipoUno,33);
-        Corredor corredorDosA = new Corredor("A",equipoUno,33);
-        Corredor corredorTresA = new Corredor("A",equipoUno,34);
-        
-        Corredor corredorUnoR = new Corredor("R",equipoDos,33);
-        Corredor corredorDosR = new Corredor("R",equipoDos,33);
-        Corredor corredorTresR = new Corredor("R",equipoDos,34);
-        
-        
-        Corredor corredorUnoN = new Corredor("V",equipoTres,33);
-        Corredor corredorDosN = new Corredor("V",equipoTres,33);
-        Corredor corredorTresN = new Corredor("V",equipoTres,34);
-        
+        //Se inicializan los objetos como Letra, objeto en comun y cantidad a recorrer
+        Corredor corredorUnoA = new Corredor(79, equipoUno, 33);
+        Corredor corredorDosA = new Corredor(79, equipoUno, 33);
+        Corredor corredorTresA = new Corredor(79, equipoUno, 34);
+
+        Corredor corredorUnoR = new Corredor(158, equipoDos, 33);
+        Corredor corredorDosR = new Corredor(158, equipoDos, 33);
+        Corredor corredorTresR = new Corredor(158, equipoDos, 34);
+
+        Corredor corredorUnoN = new Corredor(240, equipoTres, 33);
+        Corredor corredorDosN = new Corredor(240, equipoTres, 33);
+        Corredor corredorTresN = new Corredor(240, equipoTres, 34);
+
         //Equipo Uno
         corredorUnoA.start();
         corredorDosA.start();
         corredorTresA.start();
         
-        /*
         //Equipo Dos
+        
         corredorUnoR.start();
         corredorDosR.start();
         corredorTresR.start();
+
         
-        /*
         //Equipo tres
         corredorUnoN.start();
         corredorDosN.start();
-        corredorTresN.start();*/
-        
+        corredorTresN.start();
         Thread.sleep(1000);
-        synchronized(equipoUno){
+        
+        //Se emplea sincronización por bloques para desbloquear el primer wait que encuentre
+        synchronized (equipoUno) {
             equipoUno.notify();
         }
-        
-        
-        synchronized(equipoDos){
+
+        synchronized (equipoDos) {
             equipoDos.notify();
         }
-        
-        synchronized(equipoTres){
+
+        synchronized (equipoTres) {
             equipoTres.notify();
         }
+
     }
 }
--- src/main/java/Modelo/Corredor.java
+++ src/main/java/Modelo/Corredor.java
@@ -14,48 +14,113 @@ import java.util.logging.Logger;
  */
 public class Corredor extends Thread {
 
-    public int cantidadRecorrida = 0;
-    public String nombre;
-    public Object equipo;
-    public int posicionI;
- 
-
-    public static final String ANSI_RED = "\u001B[31m";
+    //Se inicializa cantidad recorrida, la cual guarda la cantidad recorrida en el run
+    private int cantidadRecorrida = 1;
+    //se declara caracteristicas del corredor como nombre, equipo y posicion de inicio
+    private int figura;
+    private Object equipo;
+    private int posicionI;
 
     public Corredor() {
     }
 
-    public Corredor(String nombre, Object team, int posicionI) {
-        this.nombre = nombre;
+    public Corredor(int figura, Object team, int posicionI) {
+        this.figura = figura;
         this.equipo = team;
         this.posicionI = posicionI;
     }
 
     @Override
     public void run() {
-        
-        synchronized (this.equipo) {
+
+        synchronized (this.getEquipo()) {
+            /*if (Thread.currentThread().getName().equals("Thread-2")) {
+                System.out.println("ok");
+            }*/
             try {
-                System.out.println("Iniciando" + this.nombre);
-                this.equipo.wait();
+                System.out.println("Inicializar" + ((char)this.getFigura()));
+                
+                this.getEquipo().wait();
 
-                while (cantidadRecorrida < posicionI) {
+                while (getCantidadRecorrida() < getPosicionI()) {
 
-                    cantidadRecorrida += (int) (Math.random() * 3);
-                    cantidadRecorrida = cantidadRecorrida > posicionI ? posicionI : cantidadRecorrida;
+                    setCantidadRecorrida(getCantidadRecorrida() + (int) (Math.random() * 2));
+                    setCantidadRecorrida(getCantidadRecorrida() > getPosicionI() ? getPosicionI() : getCantidadRecorrida());
 
-                    for (int i = 0; i < cantidadRecorrida; i++) {
+                    for (int i = 1; i < getCantidadRecorrida(); i++) {
+
                         System.out.print(" ");
                     }
-                    System.out.print(this.nombre + "\n");
+
+                    System.out.print(((char)this.getFigura())+ "\n");
+                    Thread.sleep(500);
                 }
 
-                System.out.print("|");
-                
-                this.equipo.notify();
+                System.out.print("|\n" + ((char)this.getFigura()));
+
+                this.getEquipo().notify();
+
             } catch (InterruptedException ex) {
                 Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+
     }
+    
+    /**
+     * @return the cantidadRecorrida
+     */
+    public int getCantidadRecorrida() {
+        return cantidadRecorrida;
+    }
+
+    /**
+     * @param cantidadRecorrida the cantidadRecorrida to set
+     */
+    public void setCantidadRecorrida(int cantidadRecorrida) {
+        this.cantidadRecorrida = cantidadRecorrida;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public int getFigura() {
+        return figura;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(int figura) {
+        this.figura = figura;
+    }
+
+    /**
+     * @return the equipo
+     */
+    public Object getEquipo() {
+        return equipo;
+    }
+
+    /**
+     * @param equipo the equipo to set
+     */
+    public void setEquipo(Object equipo) {
+        this.equipo = equipo;
+    }
+
+    /**
+     * @return the posicionI
+     */
+    public int getPosicionI() {
+        return posicionI;
+    }
+
+    /**
+     * @param posicionI the posicionI to set
+     */
+    public void setPosicionI(int posicionI) {
+        this.posicionI = posicionI;
+    }
+
 }
